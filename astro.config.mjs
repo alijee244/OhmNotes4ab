1633d37eeb773b80153796db7b7f73790354c055
alijee244 <alijee2k231@gmail.com>
2026-02-01 17:59:50 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,19 +9,7 @@ export default defineConfig({
 			title: 'Ohm Notes',
 			social: [{ icon: 'github', label: 'GitHub', href: '' }],
 			sidebar: [
-				{
-					label: '5th Semester',
-					items: [
-						{ label: 'NanoPhysics', slug: '5th-sem/nanophysics' },
-						{ label: 'Power Electronics II', slug: '5th-sem/power-electronics' },
-						{ label: 'Power System Analysis', slug: '5th-sem/power-system-analysis' },
-						{ label: 'Instrumentation', slug: '5th-sem/instrumentation' },
-						{ label: 'High Voltage', slug: '5th-sem/high-voltage' },
-						{ label: 'Communication Engg', slug: '5th-sem/communication-engg' },
-						{ label: 'Numerical Techniques', slug: '5th-sem/numerical-techniques' },
-						{ label: 'PYQ 5th-sem', slug: '5th-sem/pyq' },
-					],
-				},
+				
 				{
 					label: '5th Semester Notes',
 					autogenerate: { directory: '5th-sem' },
